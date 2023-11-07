ed74be5ca1334bde60037002754c1fbf4991b18a
hatim <hb1487@nyu.edu>
2023-11-07 18:05:35 -0500

    snake eater pitch

--- Oct 25/Assets/snakeEaterScript.cs
+++ Oct 25/Assets/snakeEaterScript.cs
@@ -10,19 +10,29 @@ public class snakeEaterScript : MonoBehaviour
 {
     public AudioSource audioSource;
     public BoxCollider trigger1;
-    public BoxCollider trigger2;
+    public Transform trigger2;
+    public bool exited=false;
 
     private bool playingMusic=false;
+    private float d;
+    private Transform player;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("hi");
+        d=Vector3.Distance(trigger2.position,transform.position);
+        player=GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(playingMusic && !exited){
+            audioSource.pitch=0.5f+0.5f*(1f-Vector3.Distance(player.position,transform.position)/Vector3.Distance(trigger2.position,transform.position));
+        }
+        if(exited){
+            audioSource.Stop();
+        }
     }
 
     void OnTriggerEnter(Collider other){
@@ -31,6 +41,7 @@ public class snakeEaterScript : MonoBehaviour
             Debug.Log("collided player");
             if(!playingMusic){
                 audioSource.Play(0);
+                playingMusic=true;
             }
         }
     }
